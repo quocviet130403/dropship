9651d15e9c69845153da6523fae337c17c270a67
HoangBao <hoangbao231003@gmail.com>
2022-03-13 19:39:50 +0700

    abc

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,44 @@
-home
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <title>DropShip</title>
+</head>
+
+<body>
+    <div id="wrapper">
+        <header id="header">
+            <div id="wraper-header">
+                <div class="top-bar">
+                    <div class="currency">
+                        <p>Currency: </p>
+                        <p>GBP</p> <span><i class="fa-solid fa-angle-down"></i></span>
+                    </div>
+                    <div class="register">
+                        <p>Register</p>
+                        <p>Sign In</p>
+                    </div>
+                    <div class="cart">
+                        <div class="icon-cart">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                        </div>
+                        <p>Empty</p>
+                    </div>
+                </div>
+                <div class="mid-bar">
+
+                </div>
+                <div class="bot-bar">
+
+                </div>
+            </div>
+        </header>
+    </div>
+</body>
+
+</html>
