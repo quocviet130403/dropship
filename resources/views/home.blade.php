ee4d2d5dc8b28495299f463bd54fa913a9182199
HoangBao <hoangbao231003@gmail.com>
2022-03-14 13:52:49 +0700

    Header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <title>DropShip</title>
 </head>
@@ -28,13 +27,38 @@
                             <i class="fa-solid fa-cart-shopping"></i>
                         </div>
                         <p>Empty</p>
+                        <div class="icon-down">
+                            <span><i class="fa-solid fa-angle-down"></i>
+                        </div>
                     </div>
                 </div>
                 <div class="mid-bar">
-
+                    <div class="logo">
+                        <a href=""><img src="{{asset('public/images/home-img/logo.png')}}" alt=""></a>
+                    </div>
+                    <nav>
+                        <ul class="main-menu">
+                            <li><a href="">Mens</a></li>
+                            <li><a href="">Womens</a></li>
+                            <li><a href="">The Brand</a></li>
+                            <li><a href="">Local stores</a></li>
+                            <li><a href="">look book</a></li>
+                        </ul>
+                    </nav>
+                    <div class="box-search">
+                        <form action="">
+                            <input type="text" name="search">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </form>
+                    </div>  
                 </div>
                 <div class="bot-bar">
-
+                    <div class="ave">
+                        <img src="{{asset('public/images/home-img/AVE.png')}}" alt="">
+                    </div>
+                    <button>
+                        <p>shop men's collection</p>
+                    </button>
                 </div>
             </div>
         </header>
