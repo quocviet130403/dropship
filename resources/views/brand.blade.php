3b8f04973335700561bbee64c95fbdb1cb6f3475
Huy <nnhuy.c6ntmkhai2021@gmail.com>
2022-03-16 21:02:59 +0700

    abc

---
+++ resources/views/brand.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('public/css/app.css')}}">
+
+</head>
+<body>
+    <div class="brand_begin"><img class="imgs" src="{{asset('public/images/brand_img/1.png')}}" alt=""></div>
+</body>
+</html>
